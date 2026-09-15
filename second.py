78907eff47e637e07b0b31d548d5f663c0b07cb9
UdZiRoN <rudometow06@gmail.com>
2026-09-15 08:59:21 +0700

    5 last

--- second.py
+++ second.py
@@ -2,14 +2,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 f = 2  
-t = np.linspace(0, 5 / f, 1000)
+t = np.linspace(0, 5 / f, 1000)  
 
-x = (4 / np.pi) * np.cos(2 * np.pi * f * t - np.pi / 2) + (4 / (3 * np.pi)) * np.cos(2 * np.pi * 3 * f * t - np.pi / 2)
+plt.figure(figsize=(12, 8))
+N_values = [1, 2, 3, 6] 
 
-plt.figure(figsize=(9, 4))
-plt.plot(t, x)
-plt.title('Сумма двух гармоник (Формула 4)')
-plt.xlabel('Time (s)')
-plt.ylabel('Amplitude')
-plt.grid(True)
+for idx, N in enumerate(N_values):
+    x = np.zeros_like(t)
+    for n in range(1, N + 1):
+        k = 2 * n - 1  
+        x += (4 / (k * np.pi)) * np.cos(2 * np.pi * k * f * t - np.pi / 2)
+    
+    plt.subplot(2, 2, idx + 1)
+    plt.plot(t, x)
+    plt.title(f'Число гармоник: {N}')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Amplitude')
+    plt.grid(True)
+
+plt.tight_layout()
 plt.show()
